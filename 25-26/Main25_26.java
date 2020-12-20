3a88c7b0348149589256e8a610a3da8669939fba
Vlad Kosarev <kva3292011@yandex.ru>
2020-12-20 21:08:52 +0300

    wp

---
+++ 25-26/Main25_26.java
@@ -0,0 +1,9 @@
+package ru.mirea.exercies25and26;
+
+import java.io.EOFException;
+
+public class Main25_26 {
+    public static void main(String[] args) throws EOFException {
+        Parser parser = new Parser();
+    }
+}
